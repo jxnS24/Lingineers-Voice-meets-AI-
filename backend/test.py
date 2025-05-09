66b986dfb1115f0d6d5e556b3eeaedba4a6a588d
Okan Adiguezel <okan.adiguezel@icloud.com>
2025-05-09 10:03:41 +0200

    test:model

--- backend/test.py
+++ backend/test.py
@@ -1,17 +1,1 @@
-import requests
-
-MODEL = "mistral:7b"
-
-prompt = "Hello how are you"
-
-response = requests.post(
-    "http://localhost:11434/api/generate",
-    json={
-        "model": MODEL,
-        "prompt": prompt,
-        "stream": False
-    }
-)
-
-data = response.json()
-print(data["response"])
+print("test")
