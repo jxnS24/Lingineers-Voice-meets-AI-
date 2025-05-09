1e93cdc4e011ada354c709eab4c524eefb601229
jxnS24 <167618697+jxnS24@users.noreply.github.com>
2025-05-09 09:45:10 +0200

    Test cmd

--- backend/test.py
+++ backend/test.py
@@ -1,1 +1,17 @@
-print("test")
+import requests
+
+MODEL = "mistral:7b"
+
+prompt = "Erkläre kurz den Unterschied zwischen KI und ML."
+
+response = requests.post(
+    "http://localhost:11434/api/generate",
+    json={
+        "model": MODEL,
+        "prompt": prompt,
+        "stream": False
+    }
+)
+
+data = response.json()
+print(data["response"])
