cb32ea258ee8af3813b240ae3031e197cde5330c
jxnS24 <167618697+jxnS24@users.noreply.github.com>
2025-05-23 11:12:25 +0200

    added database

---
+++ database/db_start_up.py
@@ -0,0 +1,14 @@
+from pymongo import MongoClient
+from dotenv import find_dotenv, load_dotenv
+
+import os
+
+load_dotenv(find_dotenv())
+
+with MongoClient(f'mongodb://{os.environ.get("MONGO_INITDB_ROOT_USERNAME")}:{os.environ.get("MONGO_INITDB_ROOT_PASSWORD")}@localhost:27017/') as client:
+    db = client[os.environ.get("MONGO_INITDB_DATABASE")]
+    collections = db.list_collection_names()
+
+    if "user-progress" not in collections:
+        db.create_collection("user-progress")
+
