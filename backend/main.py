6511fc2d9a7015deef61f4187936a51bd28b190b
jxnS24 <167618697+jxnS24@users.noreply.github.com>
2025-06-30 19:16:46 +0200

    refactor(data): remove level field from questions in data.json
    feat(db): add user collection creation in db_start_up.py
    
    feat(test): enhance question generation and storage logic in test.py

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,28 @@
-from fastapi import FastAPI
+import os
+
+from fastapi import FastAPI, HTTPException
 from dotenv import load_dotenv, find_dotenv
+from pymongo import MongoClient
+from fastapi.middleware.cors import CORSMiddleware
+from user import find_user, create_user
+
 import learn_vocab, json
 import uvicorn
+import bcrypt
+
+from backend.models import User
+
+load_dotenv(find_dotenv())
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/vocab/{user_id}")
 def read_root(user_id: str):
@@ -13,7 +31,19 @@ def read_root(user_id: str):
     vocab = json.loads(vocab_json)
     return vocab
 
+@app.post("/register")
+def register(user: User):
 
+    create_user(user.username, user.password)
+
+    return {"msg": "Registered successfully"}
+
+@app.post("/login")
+def login(user: User):
+
+    db_user = find_user(user.username)
+
+    return bcrypt.checkpw(user.password.encode('utf-8'), db_user['password'].encode('utf-8'))
 
 if __name__ == "__main__":
     load_dotenv(find_dotenv())
--- frontend/src/app/app-module.ts
+++ frontend/src/app/app-module.ts
@@ -5,19 +5,25 @@ import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import { LoginComponent } from './login/login';
+import {FormsModule} from '@angular/forms';
+import {HttpClient, provideHttpClient} from '@angular/common/http';
 
 @NgModule({
   declarations: [
     App,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     MatButtonModule,
     MatIconModule
   ],
   providers: [
-    provideBrowserGlobalErrorListeners()
+    provideBrowserGlobalErrorListeners(),
+    provideHttpClient()
   ],
   bootstrap: [App]
 })
